9eb5c28188c85acbd0bf55cb2904d040987cd017
Adam Szi <szi.adam@simonyi.bme.hu>
2019-11-30 13:53:32 +0100

    fireman fire

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform _transform;
+    private Rigidbody2D _rigidbody;
+
+    public float speedRatio;
+    public float speed;
+    
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Move(x, y);
+    }
+
+    private void Move(float x, float y) {
+        _rigidbody.velocity = new Vector2(x * speed * speedRatio, y * speed);
+    }
+}
